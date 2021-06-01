b85acf63f9144a39d7c299f1d6c3c17b035bab3b
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-06-01 10:43:14 +0800

    fixed scripts

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 $(function() {
-    //create a variable to monitor the input checkbox
+
     const colorSwitch = document.getElementById("input-color-switch");
     const cards = document.querySelectorAll(".project-card");
 
-    //when the input is clicked verify the state of the checkbox
     colorSwitch.addEventListener("click", checkMode);
 
-    //1. see which state the checkbox is in
-    //2. if it is checked enable the dark mode by adding the class
-    //3. if it is not checked remove dark mode by removing the class
     function checkMode() {
     console.log("checking...");
     if (colorSwitch.checked) {
